f7e544a8b667e2f635c980ebcd9f5481da091e61
Tuandev18 <phamtuan180704@gmail.com>
2026-05-11 17:36:15 +0700

    [UPDATE] remove aiFunction

--- src/routes/AIRoutes.js
+++ src/routes/AIRoutes.js
@@ -1,10 +1,8 @@
-// src/routes/ai.routes.js
 import express from "express";
 import { chatAI } from "../controller/AIController.js";
 import { authenticateOptional } from "../middlewares/authMiddleware.js";
 const router = express.Router();
 
-// Sử dụng authenticateOptional để vừa cho phép Guest, vừa nhận diện User
 router.post("/chat", authenticateOptional, chatAI);
 
 export default router;
